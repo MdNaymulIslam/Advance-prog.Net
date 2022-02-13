118270d951d6437ca69239a0c1e3fc37c4d17e46
MdNaymulIslam <naymulrm@gmail.com>
2022-02-13 22:56:10 +0600

    first commit

---
+++ MyCV/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyCV.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EducationalBackground()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult project()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Reference()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+     
+    }
+}
